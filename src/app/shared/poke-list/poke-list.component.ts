a53ca93008bb40fef3bdf4cfe8fdbc0f43467ea8
leoneDuarte <leone-duarte@outlook.com>
2021-11-24 00:13:38 -0300

    Finaliznado api de busca

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,13 +8,18 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
 
+  public getAllPokemons: any;
+
   constructor(
     private PokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
     this.PokeApiService.apiListAllPokemons.subscribe(
-      res=>console.log(res)
+      res=>{
+        this.getAllPokemons = res.results
+        console.log(this.getAllPokemons)
+      }
     );
   }
 
